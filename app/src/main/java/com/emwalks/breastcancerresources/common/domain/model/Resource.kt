8ef19a5110c2805d16e39484ccb30b6b06b91106
Emma Walker <walkere@tcd.ie>
2021-11-11 14:46:57 +0000

    remove commented out validate code for now. May revisit

--- app/src/main/java/com/emwalks/breastcancerresources/common/domain/model/Resource.kt
+++ app/src/main/java/com/emwalks/breastcancerresources/common/domain/model/Resource.kt
@@ -14,32 +14,7 @@ data class Resource(
     val tags: List<String>
     )
 
-//data class Uid(val value: Int){
-//    init {
-//        validate(value)
-//    }
-//
-//    private fun validate(uid: Int): Either<UidException, Boolean> {
-//        return when {
-//            uid.hasInvalidValue() -> Either.Left(UidException.InvalidUidException(uid))
-//            else -> Either.Right(true)
-//        }
-//    }
-//
-//    sealed class Either<out A, out B> { // 2
-//        class Left<A>(val value: A): Either<A, Nothing>()
-//        class Right<B>(val value: B): Either<Nothing, B>()
-//    }
-//
-//    sealed class UidException(message): Exception(message) { // 3
-//        data class InvalidUidException(uid: Int): UidException("$uid")
-//        data class InvalidUidFormatException(uid: Int): UidException("$uid")
-//        data class InvalidUidLengthException(uid: Int): UidException("$uid")
-//    }
-//}
-//
-//private fun Int.hasInvalidValue(): Any {
-//
-//}
+
+//do I need to validate uid?
 
 
