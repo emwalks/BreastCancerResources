741655ef5f847309e096625ee5da488de2f5ab55
Emma Walker <walkere@tcd.ie>
2021-11-11 19:28:48 +0000

    adds versioning to database and ability to use MigrationTestHelper to test for a single version migration if required in the future

--- app/src/main/java/com/emwalks/breastcancerresources/database/roomDatabase/AppDatabase.kt
+++ app/src/main/java/com/emwalks/breastcancerresources/database/roomDatabase/AppDatabase.kt
@@ -1,27 +1,28 @@
 package com.emwalks.breastcancerresources.database.roomDatabase
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverter
-import androidx.room.TypeConverters
+import androidx.room.*
+import com.emwalks.BreastCancerResourcesApplication
 import com.emwalks.breastcancerresources.database.dataAccessObjects.ResourceDao
 import com.emwalks.breastcancerresources.database.entities.Converters
 import com.emwalks.breastcancerresources.database.entities.Resource
 
-@Database(entities = [Resource::class], version = 1)
+@Database(
+    version = 1,
+    entities = [Resource::class])
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun resourceDao(): ResourceDao
 }
 
-//notes on usage
+/*
+notes on usage
+create an instance of the database
+*/
 
-// create an instance of the database
-
-//val db = Room.databaseBuilder(
-//    BreastCancerResourcesApplication.getAppContext(),
-//    AppDatabase::class.java, "breastCancerResources-database"
-//).build()
+val db = Room.databaseBuilder(
+    BreastCancerResourcesApplication.getAppContext(),
+    AppDatabase::class.java, "breastCancerResources-database")
+    .build()
 
 
 
